097ce0ae04e65ea6abba5f5c1655a66de4e25a73
Alex Poputa <poputa.alex@gmail.com>
2024-07-29 21:22:54 +0300

    Finished gallery display

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach((e)=>{
+    const li = document.createElement('li');
+    const img = `<img src='${e.preview}' alt='${e.description}' class='gallery__image'>`;
+    li.classList.add('gallery__item');
+    li.insertAdjacentHTML('afterbegin',img);
+    gallery.append(li)
+});
+
+gallery.addEventListener('click',(e)=>{
+    if (e.target.nodeName !== 'IMG'){
+        return
+    }
+});
