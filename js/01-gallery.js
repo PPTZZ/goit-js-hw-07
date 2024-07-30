e650a5f81ade406b982532be78c69610a253d850
unknown <poputa.alex@gmail.com>
2024-07-30 16:02:01 +0300

    restarted

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,16 +3,10 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
 
-galleryItems.forEach((e)=>{
-    const li = document.createElement('li');
-    const img = `<img src='${e.preview}' alt='${e.description}' class='gallery__image'>`;
-    li.classList.add('gallery__item');
-    li.insertAdjacentHTML('afterbegin',img);
-    gallery.append(li)
-});
-
-gallery.addEventListener('click',(e)=>{
-    if (e.target.nodeName !== 'IMG'){
-        return
-    }
+galleryItems.forEach((e) => {
+	const li = document.createElement('li');
+	const img = `<img src='${e.preview}' alt='${e.description}' class='gallery__image'>`;
+	li.classList.add('gallery__item');
+	li.insertAdjacentHTML('afterbegin', img);
+	gallery.append(li);
 });
