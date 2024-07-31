57d0b60dd89d5735080552c4ac851185a71df390
Alex Poputa <poputa.alex@gmail.com>
2024-07-31 20:47:58 +0300

    added  a new way to close

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,31 +4,36 @@ import { galleryItems } from './gallery-items.js';
 const gallery = document.querySelector('.gallery');
 
 // Populating gallery with tumbnails
-galleryItems.forEach((e) => {
-	const img = `<li class='gallery__item'>
+galleryItems.forEach(e => {
+  const img = `<li class='gallery__item'>
         <a class='gallery__link' href='${e.original}'>
             <img src='${e.preview}' alt='${e.description}' class='gallery__image' data-source='${e.original}'>
         </a>
     </li>`;
-	gallery.insertAdjacentHTML('beforeend', img);
+  gallery.insertAdjacentHTML('beforeend', img);
 });
 
 // Opening modal & close modal
-gallery.addEventListener('click',(e)=>{
-    e.preventDefault();
-    if (e.target.nodeName !== 'IMG'){
-        return
-    }
-    const imageLink = galleryItems.map(image => image.original)
-        .filter(image => image === e.target.dataset.source);
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const imageLink = galleryItems
+    .map(image => image.original)
+    .filter(image => image === e.target.dataset.source);
 
-  const image =  basicLightbox.create(`<img width="1400" height="900" src="${imageLink[0]}">`,{closable: false});
+  const image = basicLightbox.create(
+    `<img width="1400" height="900" src="${imageLink[0]}">`,
+    { closable: false }
+  );
   image.show();
   if (image.visible()) {
-    console.log(image.visible());
-    gallery.addEventListener('keydown',(e)=>{
-        e.target.nodeName === 'A' && e.code === 'Escape' ? image.close() : alert('Press Escape to close');
-    })
+    gallery.addEventListener('keydown', e => {
+      e.target.nodeName === 'A' && e.code === 'Escape'
+        ? image.close()
+        : _.throttle(alert('Press Escape to close'), 500);
+    });
   }
 });
 
@@ -39,4 +44,3 @@ gallery.addEventListener('click',(e)=>{
 //         e.target.nodeName === 'A' && e.code === 'Escape' ? image.remove() : alert('Press Escape to close');
 //     }
 // });
-
