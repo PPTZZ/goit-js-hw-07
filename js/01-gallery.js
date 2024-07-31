748398131e7cce2ad8667988a905a33ddc59c0a1
unknown <poputa.alex@gmail.com>
2024-07-31 16:45:21 +0300

    Finished Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,6 +3,7 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
 
+// Populating gallery with tumbnails
 galleryItems.forEach((e) => {
 	const img = `<li class='gallery__item'>
         <a class='gallery__link' href='${e.original}'>
@@ -12,7 +13,24 @@ galleryItems.forEach((e) => {
 	gallery.insertAdjacentHTML('beforeend', img);
 });
 
+// Opening modal
 gallery.addEventListener('click',(e)=>{
     e.preventDefault();
-    
-})
+    if (e.target.nodeName !== 'IMG'){
+        return
+    }
+    const imageLink = galleryItems.map(image => image.original)
+        .filter(image => image === e.target.dataset.source);
+
+   basicLightbox.create(`<img width="1400" height="900" src="${imageLink[0]}">`,{closable: false}).show();
+   
+});
+
+// Closing modal
+gallery.addEventListener('keydown',(e)=>{
+    const image = document.querySelector('.basicLightbox');
+    if(image !== null){
+        e.target.nodeName === 'A' && e.code === 'Escape' ? image.remove() : alert('Press Escape to close');
+    }
+});
+
