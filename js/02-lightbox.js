8e5a3c3694d0ae6dba33c142689bc1ec73fc5259
unknown <poputa.alex@gmail.com>
2024-08-04 12:42:09 +0300

    started task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,4 @@
-import lightbox from 'lightbox';
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
@@ -9,20 +9,15 @@ galleryItems.forEach(e => {
   const {preview, original, description} = e
   const img = `<li class='gallery__item'>
         <a class='gallery__link' href='${original}'>
-            <img src='${preview}' alt='${description}' class='gallery__image' data-source='${original}'>
+            <img src='${preview}' alt='${description}' class='gallery__image' data-source='${original}' title='${description}'>
         </a>
     </li>`;
   gallery.insertAdjacentHTML('beforeend', img);
 });
-console.log(gallery);
 
 gallery.addEventListener('click', (e)=>{
     e.preventDefault();
-
-    const imageLinks= galleryItems
-    .map(galleryItem => galleryItem.original);
-    SimpleLightbox.open({
-        items: imageLinks
-    });
+   const image = new SimpleLightbox('.gallery .gallery__link');
+   console.log(image);
 })
 
